dbece275059d49afc840438a25b5100c761c4ca8
Elian <elianhenderson.valencia.0ds@utap.edu.co>
2026-09-14 20:36:53 -0500

    primero

---
+++ examen.js
@@ -0,0 +1,12 @@
+// Datos personales
+const nombre = "Elian";
+const apellido = "Valencia";
+const edad = 22;
+const correo = "elianvalencia@example.com";
+const celular = "3153021045";
+
+// Estudio la carrera tecnico en desarrollo de software
+const carrera = "Técnico en Desarrollo de Software";
+const semestre = 2;
+const universidad = "UTAP";
+const ciudad = "Cali";
